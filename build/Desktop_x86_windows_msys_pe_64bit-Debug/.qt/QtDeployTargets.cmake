b765001e5b63691d45aca67a5b07b8e9f1371a72
Alejandro Emmanuel Álvarez Velásquez <3476932530101@ingenieria.usac.edu.gt>
2026-04-06 23:20:12 -0600

    organizacion final de carpetas y limpieza de archivos temporales

--- build/Desktop_x86_windows_msys_pe_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LenguajesProyecto1_FILE C:/Users/aleja/Documents/LenguajesProyecto1/build/Desktop_x86_windows_msys_pe_64bit-Debug/LenguajesProyecto1.exe)
-set(__QT_DEPLOY_TARGET_LenguajesProyecto1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LenguajesProyecto1_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
